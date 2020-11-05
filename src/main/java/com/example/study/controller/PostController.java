24e35b06c722b6bfa5dbc82e4f245993b8aecdbd
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-05 21:40:15 +0900

    feat : Initialize basic features of the project

---
+++ src/main/java/com/example/study/controller/PostController.java
@@ -0,0 +1,27 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+    // @RequestMapping(method = RequestMethod.POST, path = "/postMethod") // 아래와 동일함
+    // http post body -> data
+    // json, xml, multipart-form, text-plain
+    @PostMapping(value = "/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam) {
+        return searchParam;
+    }
+
+    @PutMapping("/putMethod")
+    public void put() {
+
+    }
+
+    @PatchMapping("/patchMethod")
+    public void patch() {
+
+    }
+}
