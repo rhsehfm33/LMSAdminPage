0a755ed332a4a5b04f131dd83fb87aaeef789507
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-08 02:28:14 +0900

    feat : Implement all entity classes

---
+++ src/main/java/com/example/study/model/entity/Item.java
@@ -0,0 +1,33 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+    private String name;
+    private String title;
+    private String content;
+    private Integer price;
+    private String brandName;
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+// @Table(name = "user")    // 클래스의 이름과 테이블의 이름이 같으면 따로 선언해줄 필요 없음. 단, 이 경우 @Entity가 선언되어있어야 함
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String account;
+    private String password;
+    private String status;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
