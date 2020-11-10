558487c634a80b8f8eda81d6234f3c6d6526ca7f
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-10 16:09:10 +0900

    feat : Implement basic service & controller of OrderGroup entity

---
+++ src/main/java/com/example/study/service/OrderGroupApiLogicService.java
@@ -0,0 +1,59 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.OrderGroup;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.OrderGroupApiRequest;
+import com.example.study.model.network.response.OrderGroupApiResponse;
+import com.example.study.repository.OrderGroupRepository;
+import com.example.study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+    @Autowired
+    OrderGroupRepository orderGroupRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Header<OrderGroupApiResponse> create(Header<OrderGroupApiRequest> request) {
+
+        return null;
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<OrderGroupApiResponse> response(OrderGroup orderGroup) {
+        OrderGroupApiResponse body = OrderGroupApiResponse.builder()
+                .id(orderGroup.getId())
+                .status(orderGroup.getStatus())
+                .orderType(orderGroup.getOrderType())
+                .revAddress(orderGroup.getRevAddress())
+                .revName(orderGroup.getRevName())
+                .paymentType(orderGroup.getPaymentType())
+                .totalPrice(orderGroup.getTotalPrice())
+                .totalQuantity(orderGroup.getTotalQuantity())
+                .orderAt(orderGroup.getOrderAt())
+                .arrivalDate(orderGroup.getArrivalDate())
+                .userId(orderGroup.getId())
+                .build();
+        return Header.OK(body);
+    }
+}
