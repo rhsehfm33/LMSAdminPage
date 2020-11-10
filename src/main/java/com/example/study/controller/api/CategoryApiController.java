0292ffc22fcde013a562df78dc3d6c783d35417e
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-10 20:50:03 +0900

    feat : Implement basic service & controller of Category entity

---
+++ src/main/java/com/example/study/controller/api/CategoryApiController.java
@@ -0,0 +1,41 @@
+package com.example.study.controller.api;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.CategoryApiRequest;
+import com.example.study.model.network.response.CategoryApiResponse;
+import com.example.study.service.CategoryApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryApiController implements CrudInterface<CategoryApiRequest, CategoryApiResponse> {
+
+    @Autowired
+    CategoryApiLogicService categoryApiLogicService;
+
+    @PostMapping("")
+    @Override
+    public Header<CategoryApiResponse> create(@RequestBody Header<CategoryApiRequest> request) {
+        return null;
+    }
+
+    @GetMapping("{id}")
+    @Override
+    public Header<CategoryApiResponse> read(@PathVariable Long id) {
+        return null;
+    }
+
+    @PutMapping("")
+    @Override
+    public Header<CategoryApiResponse> update(@RequestBody Header<CategoryApiRequest> request) {
+        return null;
+    }
+
+    @DeleteMapping("{id}")
+    @Override
+    public Header delete(@PathVariable Long id) {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/study/service/CategoryApiLogicService.java
@@ -0,0 +1,48 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.Category;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.CategoryApiRequest;
+import com.example.study.model.network.response.CategoryApiResponse;
+import com.example.study.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryApiLogicService implements CrudInterface<CategoryApiRequest, CategoryApiResponse> {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Override
+    public Header<CategoryApiResponse> create(Header<CategoryApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header<CategoryApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<CategoryApiResponse> update(Header<CategoryApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<CategoryApiResponse> response(Category category) {
+
+        CategoryApiResponse body = CategoryApiResponse.builder()
+                .id(category.getId())
+                .type(category.getType())
+                .title(category.getTitle())
+                .build();
+
+        return Header.OK(body);
+    }
+}
