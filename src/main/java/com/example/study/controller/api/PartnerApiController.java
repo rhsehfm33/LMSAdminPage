5a94615db4509f88c044e37dcb325d37ef74b805
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-10 22:02:32 +0900

    feat : Implement PartnerApiController & PartnerApiLogicService delete method

--- src/main/java/com/example/study/controller/api/PartnerApiController.java
+++ src/main/java/com/example/study/controller/api/PartnerApiController.java
@@ -36,6 +36,6 @@ public class PartnerApiController implements CrudInterface<PartnerApiRequest, Pa
     @DeleteMapping("{id}")
     @Override
     public Header delete(@PathVariable Long id) {
-        return null;
+        return partnerApiLogicService.delete(id);
     }
 }
--- src/main/java/com/example/study/service/PartnerApiLogicService.java
+++ src/main/java/com/example/study/service/PartnerApiLogicService.java
@@ -80,7 +80,12 @@ public class PartnerApiLogicService implements CrudInterface<PartnerApiRequest, 
 
     @Override
     public Header delete(Long id) {
-        return null;
+        return partnerRepository.findById(id)
+                .map(partner -> {
+                    partnerRepository.delete(partner);
+                    return Header.OK();
+                })
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
     }
 
     private Header<PartnerApiResponse> response(Partner partner) {
