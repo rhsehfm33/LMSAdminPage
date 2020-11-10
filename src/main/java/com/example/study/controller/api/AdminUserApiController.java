5d846004bbc507fb5022a1545dfc50ed17a45a32
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-10 19:35:39 +0900

    feat : Implement basic service & controller of AdminUser entity

---
+++ src/main/java/com/example/study/controller/api/AdminUserApiController.java
@@ -0,0 +1,41 @@
+package com.example.study.controller.api;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.AdminUserApiRequest;
+import com.example.study.model.network.response.AdminUserApiResponse;
+import com.example.study.service.AdminUserApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/adminUser")
+public class AdminUserApiController implements CrudInterface<AdminUserApiRequest, AdminUserApiResponse> {
+
+    @Autowired
+    AdminUserApiLogicService adminUserApiLogicService;
+
+    @PostMapping("")
+    @Override
+    public Header<AdminUserApiResponse> create(@RequestBody Header<AdminUserApiRequest> request) {
+        return null;
+    }
+
+    @GetMapping("{id}")
+    @Override
+    public Header<AdminUserApiResponse> read(@PathVariable Long id) {
+        return null;
+    }
+
+    @PutMapping("")
+    @Override
+    public Header<AdminUserApiResponse> update(@RequestBody Header<AdminUserApiRequest> request) {
+        return null;
+    }
+
+    @DeleteMapping("{id}")
+    @Override
+    public Header delete(@PathVariable Long id) {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/study/service/AdminUserApiLogicService.java
@@ -0,0 +1,54 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.AdminUser;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.AdminUserApiRequest;
+import com.example.study.model.network.response.AdminUserApiResponse;
+import com.example.study.repository.AdminUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminUserApiLogicService implements CrudInterface<AdminUserApiRequest, AdminUserApiResponse> {
+
+    @Autowired
+    AdminUserRepository adminUserRepository;
+
+    @Override
+    public Header<AdminUserApiResponse> create(Header<AdminUserApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header<AdminUserApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<AdminUserApiResponse> update(Header<AdminUserApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<AdminUserApiResponse> response(AdminUser adminUser) {
+
+        AdminUserApiResponse body = AdminUserApiResponse.builder()
+                .account(adminUser.getAccount())
+                .password(adminUser.getPassword())
+                .status(adminUser.getStatus())
+                .role(adminUser.getRole())
+                .lastLoginAt(adminUser.getLastLoginAt())
+                .loginFailCount(adminUser.getLoginFailCount())
+                .passwordUpdatedAt(adminUser.getPasswordUpdatedAt())
+                .registeredAt(adminUser.getRegisteredAt())
+                .unregisteredAt(adminUser.getUnregisteredAt())
+                .build();
+
+        return Header.OK(body);
+    }
+}
