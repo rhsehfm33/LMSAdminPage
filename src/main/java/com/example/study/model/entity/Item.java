952488ab4265e60ffd93f6420ab62e2282eb8e6b
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-11 01:39:27 +0900

    feat : Implement status enum in Item entity

--- src/main/java/com/example/study/model/entity/Item.java
+++ src/main/java/com/example/study/model/entity/Item.java
@@ -1,5 +1,6 @@
 package com.example.study.model.entity;
 
+import com.example.study.model.enumclass.ItemStatus;
 import lombok.*;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.CreatedBy;
@@ -22,17 +23,28 @@ import java.util.List;
 @Builder
 @Accessors(chain = true)
 public class Item {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String status;
+
+    @Enumerated(EnumType.STRING)
+    private ItemStatus status; // 등록 / 해지 / 검수중(등록대기중)
+
     private String name;
+
     private String title;
+
     private String content;
+
     private BigDecimal price;
+
     private String brandName;
+
     private LocalDateTime registeredAt;
+
     private LocalDateTime unregisteredAt;
+
 
     @CreatedDate
     private LocalDateTime createdAt;
